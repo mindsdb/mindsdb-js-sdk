ebde467f023596dbc6135bd6f6ee31e7c9bed583
tmichaeldb <tyler@mindsdb.com>
2023-02-14 15:58:19 -0800

    Implemented database operations for core SDK using REST API

---
+++ src/databases/databasesApiClient.ts
@@ -0,0 +1,39 @@
+import { JsonValue } from '../util/json';
+import Database from './database';
+
+/**
+ * Abstract class outlining Database API operations supported by the SDK.
+ */
+export default abstract class DatabasesApiClient {
+  /**
+   * Gets all databases for the authenticated user.
+   * @returns {Promise<Array<Database>>} - All databases for the user.
+   */
+  abstract getAllDatabases(): Promise<Array<Database>>;
+
+  /**
+   * Gets a database by name for the authenticated user.
+   * @param {string} name - Name of the database.
+   * @returns {Promise<Database | undefined>} - Matching database, or undefined if it doesn't exist.
+   */
+  abstract getDatabase(name: string): Promise<Database | undefined>;
+
+  /**
+   * Creates a database with the given name, engine, and parameters.
+   * @param {string} name - Name of the database to be created.
+   * @param {string} [engine] - Optional name of the database engine.
+   * @param {string} [params] - Optional parameters used to connect to the database (e.g. user, password).
+   * @returns {Promise<Database>} - Newly created database.
+   */
+  abstract createDatabase(
+    name: string,
+    engine?: string,
+    params?: Record<string, JsonValue>
+  ): Promise<Database>;
+
+  /**
+   * Deletes a database by name.
+   * @param {string} name - Name of the database to be deleted.
+   */
+  abstract deleteDatabase(name: string): Promise<void>;
+}
---
+++ tests/databases/databasesRestApiClient.test.ts
@@ -0,0 +1,140 @@
+import axios from 'axios';
+import DatabasesRestApiClient from '../../src/databases/databasesRestApiClient';
+import SqlRestApiClient from '../../src/sql/sqlRestApiClient';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock('../../src/sql/sqlRestApiClient');
+const mockedSqlRestApiClient = new SqlRestApiClient(
+  mockedAxios
+) as jest.Mocked<SqlRestApiClient>;
+
+describe('Testing Databases REST API client', () => {
+  afterEach(() => {
+    mockedSqlRestApiClient.runQuery.mockClear();
+  });
+  test('should get database', async () => {
+    const databasesRestApiClient = new DatabasesRestApiClient(
+      mockedSqlRestApiClient
+    );
+    const databaseRows = [
+      { database: 'test_db', type: 'data', engine: 'files' },
+      { database: 'not_the_db', type: 'project', engine: undefined },
+      { database: 'also_not_the_db', type: 'data', engine: 'postgres' },
+    ];
+    mockedSqlRestApiClient.runQuery.mockImplementation(() => {
+      return Promise.resolve({
+        columnNames: [],
+        context: {
+          db: 'mindsdb',
+        },
+        type: 'ok',
+        rows: databaseRows,
+      });
+    });
+
+    const database = await databasesRestApiClient.getDatabase('test_db');
+
+    const actualQuery = mockedSqlRestApiClient.runQuery.mock.calls[0][0];
+    const expectedQuery = `SHOW FULL DATABASES`;
+    expect(actualQuery).toEqual(expectedQuery);
+
+    expect(database?.name).toEqual('test_db');
+    expect(database?.type).toEqual('data');
+    expect(database?.engine).toEqual('files');
+  });
+
+  test('should get all databases', async () => {
+    const databasesRestApiClient = new DatabasesRestApiClient(
+      mockedSqlRestApiClient
+    );
+    const databaseRows = [
+      { database: 'test_db', type: 'data', engine: 'files' },
+      { database: 'test_db2', type: 'project', engine: undefined },
+    ];
+    mockedSqlRestApiClient.runQuery.mockImplementation(() => {
+      return Promise.resolve({
+        columnNames: [],
+        context: {
+          db: 'mindsdb',
+        },
+        type: 'ok',
+        rows: databaseRows,
+      });
+    });
+
+    const databases = await databasesRestApiClient.getAllDatabases();
+
+    const actualQuery = mockedSqlRestApiClient.runQuery.mock.calls[0][0];
+    const expectedQuery = `SHOW FULL DATABASES`;
+    expect(actualQuery).toEqual(expectedQuery);
+
+    expect(databases).toHaveLength(2);
+    expect(databases[0].name).toEqual('test_db');
+    expect(databases[0].type).toEqual('data');
+    expect(databases[0].engine).toEqual('files');
+    expect(databases[1].name).toEqual('test_db2');
+    expect(databases[1].type).toEqual('project');
+    expect(databases[1].engine).toBeUndefined();
+  });
+
+  test('should create database', async () => {
+    const databasesRestApiClient = new DatabasesRestApiClient(
+      mockedSqlRestApiClient
+    );
+    mockedSqlRestApiClient.runQuery.mockImplementation(() => {
+      return Promise.resolve({
+        columnNames: [],
+        context: {
+          db: 'mindsdb',
+        },
+        type: 'ok',
+        rows: [],
+      });
+    });
+
+    const params = {
+      user: 'test_user',
+      password: 'test_password',
+      port: 9001,
+      args: { nested: true },
+    };
+    const database = await databasesRestApiClient.createDatabase(
+      'test_db',
+      'test_engine',
+      params
+    );
+
+    const actualQuery = mockedSqlRestApiClient.runQuery.mock.calls[0][0];
+    const expectedQuery = `CREATE DATABASE test_db
+WITH ENGINE = 'test_engine',
+PARAMETERS = {"user":"test_user","password":"test_password","port":9001,"args":{"nested":true}}`;
+    expect(actualQuery).toEqual(expectedQuery);
+
+    expect(database.name).toEqual('test_db');
+    expect(database.type).toEqual('data');
+    expect(database.engine).toEqual('test_engine');
+  });
+
+  test('should delete database', async () => {
+    const databasesRestApiClient = new DatabasesRestApiClient(
+      mockedSqlRestApiClient
+    );
+    mockedSqlRestApiClient.runQuery.mockImplementation(() => {
+      return Promise.resolve({
+        columnNames: [],
+        context: {
+          db: 'mindsdb',
+        },
+        type: 'ok',
+        rows: [],
+      });
+    });
+
+    await databasesRestApiClient.deleteDatabase('test_db');
+
+    const actualQuery = mockedSqlRestApiClient.runQuery.mock.calls[0][0];
+    const expectedQuery = `DROP DATABASE \`test_db\``;
+    expect(actualQuery).toEqual(expectedQuery);
+  });
+});
