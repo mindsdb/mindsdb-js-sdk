552f6336ed65cabea4eb8d53fa2914c15e25f89b
Niharika Goulikar <goulikarniharika@gmail.com>
2024-10-19 07:28:28 +0000

    Added Create view

--- src/views/view.ts
+++ src/views/view.ts
@@ -12,17 +12,21 @@ export default class View {
 
   /** Project the view belongs to. */
   project: string;
-
+  
+  /** SELECT statement to use for initializing the view. */
+  select:string;
   /**
    *
    * @param {ViewsApiClient} viewsApiClient - API client to use for executing view operations.
    * @param {string} name - Name of the view.
    * @param {string} project - Project the view belongs to.
+   * @param {string} select - SELECT statement to use for initializing the view.
    */
-  constructor(viewsApiClient: ViewsApiClient, name: string, project: string) {
+  constructor(viewsApiClient: ViewsApiClient, name: string, project: string,select:string) {
     this.viewsApiClient = viewsApiClient;
     this.name = name;
     this.project = project;
+    this.select=select;
   }
 
   /** Deletes this view from the project it belongs to.
@@ -30,5 +34,12 @@ export default class View {
    */
   async delete(): Promise<void> {
     await this.viewsApiClient.deleteView(this.name, this.project);
+  }
+
+  /** Creates a view for the given project
+   *  @throws {MindsDbError} - Something went wrong creating this view.
+   */
+  async create():Promise<void>{
+    await this.viewsApiClient.createView(this.name,this.project,this.select);
   }
 }
